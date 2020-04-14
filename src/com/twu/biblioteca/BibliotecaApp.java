bc06af9cbfdcd2ed0591c9542481c1f2c6e7a73b
gerardfabrego <gerardfabrego@gmail.com>
2020-04-14 13:04:13 +0200

    Welcome message and list of books

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,8 +1,21 @@
 package com.twu.biblioteca;
 
 public class BibliotecaApp {
+    static String[] booksList = new String[]{"Books", "To kill a mockingbird", "The wise man's fear", "1984", "Ulysses"};
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        printWelcomeMessage();
+        printListOfBooks();
+    }
+
+
+    public static void printWelcomeMessage(){
+        System.out.println("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!");
+    }
+
+    public static void printListOfBooks() {
+        for(String book: booksList){
+            System.out.println(book);
+        }
     }
 }
--- test/com/twu/biblioteca/ExampleTest.java
+++ test/com/twu/biblioteca/ExampleTest.java
@@ -1,13 +1,36 @@
 package com.twu.biblioteca;
 
-
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+import java.io.*;
 
 public class ExampleTest {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    @Before
+    public void setUpStream() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @After
+    public void restoreStream() {
+        System.setOut(originalOut);
+    }
 
     @Test
-    public void test() {
-        assertEquals(1, 1);
+    public void testPrintWelcomeMessage() {
+        //BibliotecaApp.main(new String[]{});
+        BibliotecaApp.printWelcomeMessage();
+        assertEquals("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!\n", outContent.toString());
+    }
+
+    @Test
+    public void testPrintListOfBooks() {
+        BibliotecaApp.printListOfBooks();
+        assertEquals("Books\nTo kill a mockingbird\nThe wise man's fear\n1984\nUlysses\n", outContent.toString());
+
     }
 }
